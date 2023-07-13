d3dbd7d6153318b8d1e80b3fd211f0381e8e893a
Brett St. George <brett.stgeorge@gmail.com>
2023-07-13 12:54:56 -0600

    product load complete and fully functional.

--- server/ETL.js
+++ server/ETL.js
@@ -1,35 +1,67 @@
 const fs = require('fs');
 const util = require('util');
 const Post = require('./models.js')
+const Promise = require("bluebird");
 
-let data = '';
+let productData = '';
+let questionData = '';
+let answerData = '';
 
 const productsFile = '/Users/brett.stgeorge/Downloads/product.csv';
+const questionsFile = '/Users/brett.stgeorge/Downloads/questions.csv';
+const answersFile = '/Users/brett.stgeorge/Downloads/answers.csv';
 
+const readStream = {};
+readStream.products = fs.createReadStream(productsFile, {encoding: 'utf8'});
+// readStream.questions = fs.createReadStream(productsFile, {encoding: 'utf8'});
+// readStream.answers = fs.createReadStream(productsFile, {encoding: 'utf8'});
 
-const readStream = fs.createReadStream(productsFile, {encoding: 'utf8'});
+const rs = Promise.promisifyAll(readStream, { multiArgs: true });
 
-readStream.on('data', (chunk) => {
-  data += chunk;
+rs.products.on('data', (chunk) => {
+  productData += chunk;
 })
+// rs.questions.on('data', (chunk) => {
+//   data += chunk;
+// })
+// rs.answers.on('data', (chunk) => {
+//   data += chunk;
+// })
 
-readStream.on('end', () => {
+rs.products.on('end', () => {
   let arrObj = [];
-  let lines = data.split('\n');
+  let lines = productData.split('\n');
   let headers = lines[0].split(',');
-  // arrObj[0] = headers;
-  for (let r = 0; r < lines.length; r++) {
-    let rowData = lines[r].split(',')
-    arrObj[r] = {};
-    // objElement = {};
+  for (let r = 1; r < lines.length; r++) {
+    let rowData = lines[r].split(',"').join('~');
+    rowData = rowData.split('","').join('~');
+    rowData = rowData.split('",').join('~');
+    rowData = rowData.split('~');
+    arrObj[r - 1] = {};
     for(let c = 0; c < headers.length; c++) {
-      arrObj[r][headers[c]] = rowData[c].split('\'').join('`').split('\"').join('');
-      // objElement[headers[c]] = rowData[c].split('\'').join('`').split('\"').join('');
+      arrObj[r - 1][headers[c]] = rowData[c].split('\'').join('`').split('\"').join('');
     }
-    // arrObj.push(JSON.stringify(objElement))
   }
   Post.addAllProducts(arrObj);
 })
+
+// readStreamQuestions.on('end', () => {
+//   let arrObj = [];
+//   let lines = data.split('\n');
+//   let headers = lines[0].split(',');
+//   // arrObj[0] = headers;
+//   for (let r = 0; r < lines.length; r++) {
+//     let rowData = lines[r].split(',')
+//     arrObj[r] = {};
+//     // objElement = {};
+//     for(let c = 0; c < headers.length; c++) {
+//       arrObj[r][headers[c]] = rowData[c].split('\'').join('`').split('\"').join('');
+//       // objElement[headers[c]] = rowData[c].split('\'').join('`').split('\"').join('');
+//     }
+//     // arrObj.push(JSON.stringify(objElement))
+//   }
+//   Post.addAllQuestions(arrObj);
+// })
 
 
 
--- server/models.js
+++ server/models.js
@@ -34,76 +34,48 @@ exports.addProduct = ({id, name, slogan, description, category, default_price}) 
 }
 
 exports.addAllProducts = (arrObj) => {
+  db.query(`DROP TABLE IF EXISTS products;`)
 
-  // let dropTableProducts = () => {
-    db.query(`DROP TABLE IF EXISTS products;`)
-    // , (err,res) => {
-    //   if (err) {
-    //     console.log(err);
-    //   } else {
-    //     console.log('table products dropped if existed.');
-    //   }
-    // })
-  // }
-
-  // let createTableProducts = () => {
-    .then(() => {
+  .then(() => {
     console.log('table products dropped if existed.')
-    return db.query(`CREATE TABLE products (id VARCHAR(255), name VARCHAR(255), slogan VARCHAR(255), description VARCHAR(1023), category VARCHAR(255), default_price VARCHAR(255));`)
-    // , (err,res) => {
-    //   if (err) {
-    //     console.log(err);
-    //   } else {
-    //     console.log('table products created.')
-    //   }
-    // })
-  // }
-})
+    db.query(`CREATE TABLE products (product_id integer PRIMARY KEY, name VARCHAR(255), slogan VARCHAR(255), description VARCHAR(1023), category VARCHAR(255), default_price integer);`)
+  })
 
-  // let populateTableProducts = (arrObj) => {
-    .then(() => {
+  .then(() => {
     console.log('table products created.')
-    return db.query(`INSERT INTO products (id, name, slogan, description, category, default_price) SELECT * FROM json_to_recordset('${JSON.stringify(arrObj)}') as x(id VARCHAR(255), name VARCHAR(255), slogan VARCHAR(255), description VARCHAR(1023), category VARCHAR(255), default_price VARCHAR(255));`)
-    // , (err, res) => {
-    //   if (err) {
-    //     console.log(err);
-    //   } else {
-    //     db.query('SELECT COUNT(*) FROM products;', (err, res) => {
-    //       if (err) {
-    //         console.log(err)
-    //       } else {
-    //         console.log(`${res.rows[0].count} records added successfully`)
-    //         db.end();
-    //       }
-    //     })
-    //   }
-    // });
-  // }
-})
-.then(() => {
-  console.log('records added successfully.')
-  db.end();
-})
-.catch((err) => {
-  console.log(err);
-})
+    return db.query(`INSERT INTO products (product_id, name, slogan, description, category, default_price) SELECT * FROM json_to_recordset('${JSON.stringify(arrObj)}') as x(id integer, name VARCHAR(255), slogan VARCHAR(255), description VARCHAR(1023), category VARCHAR(255), default_price integer);`)
+  })
 
-  // dropTableProducts();
-  // createTableProducts();
-  // populateTableProducts(arrObj);
+  .then((res) => {
+    console.log(`${res.rowCount} records added successfully to products table.`)
+    db.end();
+  })
 
-  // dropTableProducts().then((res) => {
-  //   .then((arrObj) => {
-  //   console.log('table products dropped if existed.');
-  //   createTableProducts();
-  // }).then((res) => {
-  //   console.log('table products created.');
-  //   populateTableProducts(arrObj);
-  // }).then((res) => {
-  //   console.log(`${res.rows[0].count} records added successfully`);
-  //   db.end();
-  // }).catch((err) => {
-  //   console.log(err);
-  // })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+exports.addAllQuestions = (arrObj) => {
+  db.query(`DROP TABLE IF EXISTS questions;`)
+
+  .then(() => {
+    console.log('table questions dropped if existed.')
+    db.query(`CREATE TABLE questions (question_id VARCHAR(255) PRIMARY KEY, product_id VARCHAR(255) REFERENCES products (product_id), body VARCHAR(255), date_written VARCHAR(255), asker_name VARCHAR(1023), asker_email VARCHAR(255), reported BOOLEAN, helpful integer);`)
+  })
+
+  .then(() => {
+    console.log('table questions created.')
+    return db.query(`INSERT INTO questions (question_id, product_id, body, date_written, asker_name, asker_email, reported, helpful) SELECT * FROM json_to_recordset('${JSON.stringify(arrObj)}') as x(question_id VARCHAR(255) PRIMARY KEY, product_id VARCHAR(255) REFERENCES products (product_id), body VARCHAR(255), date_written VARCHAR(255), asker_name VARCHAR(1023), asker_email VARCHAR(255), reported BOOLEAN, helpful integer);`)
+  })
+
+  .then((res) => {
+    console.log(`${res.rowCount} records added successfully to questions table.`)
+    db.end();
+  })
+
+  .catch((err) => {
+    console.log(err);
+  })
 }
 
