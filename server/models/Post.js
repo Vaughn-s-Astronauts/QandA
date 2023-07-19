d361d42c6980268b27be34b99c04e7cd2445ceb8
Brett St. George <130505873+bstgeorge@users.noreply.github.com>
2023-07-19 14:02:12 -0600

    Update Post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -8,23 +8,25 @@ const Promise = require("bluebird");
 
 const populate = {};
 
-exports.getProducts = () => {
-  return db.query("SELECT * FROM products;");
+exports.getProducts = (page, count) => {
+  return db.query(`SELECT * FROM products WHERE product_id BETWEEN ${(page - 1) * count + 1} AND ${(page - 1) * count + count};`);
 }
 
 exports.getProduct = (product_id) => {
   return db.query(`SELECT * FROM products WHERE product_id=${product_id};`);
 }
 
-exports.getQuestions = (product_id) => {
-  return db.query(`SELECT * FROM questions WHERE product_id=${product_id};`);
+exports.getQuestions = (product_id, page, count) => {
+  console.log('product_id: ', product_id);
+  console.log('page: ', page);
+  console.log('count: ', count);
+  return db.query(`SELECT * FROM questions WHERE product_id=${product_id} LIMIT ${count};`);
 }
 
-exports.getAnswers = (question_id) => {
-  return db.query(`SELECT * FROM answers WHERE question_id=${question_id};`)
+exports.getAnswers = (question_id, page, count) => {
+  return db.query(`SELECT * FROM answers WHERE question_id=${question_id} LIMIT ${count};`)
 }
 
 exports.addProduct = ({id, name, slogan, description, category, default_price}) => {
   return db.query(`INSERT INTO products VALUES (${id}, {${name}, ${slogan}, ${description}, ${category}, ${default_price})`);
 }
-
